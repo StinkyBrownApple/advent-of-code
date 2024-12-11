858c0bd4861deb0ad79a26f4425162cb2787f217
Steven Lacey <steven.lacey@bjss.com>
2024-12-11 19:36:33 +0000

    day 11 v5 - puzzle solved

--- aoc_2024/src/day_11/day_11.rs
+++ aoc_2024/src/day_11/day_11.rs
@@ -1,55 +1,58 @@
+use std::collections::HashMap;
 use std::time::Instant;
-use rayon::prelude::*;
 
 fn get_input() -> String {
     String::from(include_str!("./input.txt"))
 }
 
 pub fn day_eleven_part_one() {
-    day_eleven(25); // Finished 25 iters in 31.9875ms. Answer: 218079
+    day_eleven(25); // Finished 25 iters in 5.6995ms. Answer: 218079
 }
 
 pub fn day_eleven_part_two() {
-    day_eleven(40); // Finished 40 iters in 3.653700459s. Answer: 115133671
+    day_eleven(75); // Finished 75 iters in 145.72825ms. Answer: 259755538429618
 }
 
-pub fn day_eleven(iters: usize) {
-    let mut stones = get_input()
+fn day_eleven(iters: usize) {
+    let stones = get_input()
         .trim()
         .split_whitespace()
         .map(|s| s.parse::<usize>().unwrap())
         .collect::<Vec<usize>>();
-
-    let full_time = Instant::now();
-    for i in 0..iters {
+    
+    let mut total: usize = 0;
+    let start_time = Instant::now();
+    let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
+    for stone in stones {
         let loop_time = Instant::now();
-        stones = stones
-            .par_chunks_mut(1000)
-            .map(|chunk| {
-                let mut new_stones: Vec<usize> = vec![];
-                chunk.iter_mut().for_each(|j| {
-                    if *j == 0 {
-                        *j = 1;
-                        return;
-                    }
-                let len = j.to_string().len() as u32;
-                if len % 2 == 0 {
-                    let shift = 10usize.pow(len / 2);
-                    let split_a = *j / shift;
-                    let split_b = *j % shift;
-                    *j = split_a;
-                    new_stones.push(split_b);
-                    return;
-                }
-                *j = *j * 2024;
-                });
-                let mut concat_stones = chunk.iter().map(|x| *x).collect::<Vec<usize>>();
-                concat_stones.append(&mut new_stones);
-                return concat_stones;
-            })
-            .flatten()
-            .collect::<Vec<usize>>();
-        println!("Loop {} took {:?}. Stone count: {:?}", i, loop_time.elapsed(), stones.len());
+        total += recurse(stone, 0, iters, &mut cache);
+        println!("Stone {} took {:?}. Current stone count: {:?}", stone, loop_time.elapsed(), total);
+
     }
-    println!("Finished {} iters in {:?}. Answer: {}", iters, full_time.elapsed(), stones.len());
+    println!("Finished {} iters in {:?}. Answer: {}", iters, start_time.elapsed(), total);
+}
+
+fn recurse(num: usize, iteration: usize, target_iteration: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
+    if cache.contains_key(&(num, iteration)) {
+        return *cache.get(&(num, iteration)).unwrap();
+    }
+    if iteration == target_iteration {
+        return 1;
+    }
+    if num == 0 {
+        let cache_val = recurse(1, iteration + 1, target_iteration, cache);
+        cache.insert((num, iteration), cache_val);
+        return cache_val;
+    }
+    let len = num.to_string().len() as u32;
+    if len % 2 == 0 {
+        let shift = 10usize.pow(len / 2);
+        let split_a = num / shift;
+        let split_b = num % shift;
+        let cache_a = recurse(split_a, iteration + 1, target_iteration, cache);
+        let cache_b = recurse(split_b, iteration + 1, target_iteration, cache);
+        cache.insert((num, iteration), cache_a + cache_b);
+        return cache_a + cache_b;
+    }
+    recurse(num * 2024, iteration + 1, target_iteration, cache)
 }
